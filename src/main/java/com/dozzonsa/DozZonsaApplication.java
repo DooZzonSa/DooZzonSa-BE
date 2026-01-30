fd43d7e58ba26070b81333b28f5f7f892ad897fe
시원 <cnc4750@inha.edu>
2026-01-30 17:53:06 +0900

    init: 스프링부트 프로젝트 초기 세팅

---
+++ src/main/java/com/dozzonsa/DozZonsaApplication.java
@@ -0,0 +1,12 @@
+package com.dozzonsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DozZonsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DozZonsaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dozzonsa/DozZonsaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dozzonsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DozZonsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
