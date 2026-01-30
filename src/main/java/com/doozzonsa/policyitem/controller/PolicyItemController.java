cdba98cd425611a76f1f395ed6847e468fa69909
seahee <lollol0617@gmail.com>
2026-01-31 00:40:11 +0900

    [feat] Get 매핑을 Post로 수정(#7)

--- src/main/java/com/doozzonsa/policyitem/controller/PolicyItemController.java
+++ src/main/java/com/doozzonsa/policyitem/controller/PolicyItemController.java
@@ -1,7 +1,7 @@
 package com.doozzonsa.policyitem.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class PolicyItemController {
 
 	private final PolicyAiService policyAiService;
 
-	@GetMapping
+	@PostMapping
 	ResponseEntity<SuccessResponse<PolicyAnalysisResponse>> analysisPolicy(
 		@Valid @RequestBody PolicyAnalysisRequest request
 	) {
