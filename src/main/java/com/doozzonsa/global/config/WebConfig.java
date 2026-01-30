b17c546cab4864a2121d5d225814cf28c71a4fec
sehee <132613722+sae2say@users.noreply.github.com>
2026-01-30 20:26:38 +0900

    [feat] webConfig 추가, openai 의존성 추가 (#6)
    * [chore] yml, WebConfig 작성(#4)
    
    * [chore] yml 삭제(#4)
    
    * [chore] openai 의존성 추가(#4)

---
+++ src/main/java/com/doozzonsa/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.doozzonsa.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+			.allowedHeaders("*")
+			.allowCredentials(false);
+	}
+
+}
