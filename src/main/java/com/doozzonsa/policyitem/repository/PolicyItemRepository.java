370f68887ebe8535f3ad1ad59f4deb8956a0fa75
시원 <cnc4750@inha.edu>
2026-01-31 01:18:17 +0900

    fix: PolicyItemRepository 하나로 통합

--- src/main/java/com/doozzonsa/policyitem/repository/PolicyItemRepository.java
+++ src/main/java/com/doozzonsa/policyitem/repository/PolicyItemRepository.java
@@ -1,7 +1,10 @@
 package com.doozzonsa.policyitem.repository;
 
 import com.doozzonsa.policyitem.domain.PolicyItem;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PolicyItemRepository extends JpaRepository<PolicyItem, Long> {
+
+    Optional<PolicyItem> findByName(String name);
 }
